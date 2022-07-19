df8a73199b23d04a097395e8ffac1bf305c157a6
Nastasia-Lis <nastyalis2012@mail.ru>
2022-07-19 09:10:25 +0500

    first in-work

--- src/js/modules/table.js
+++ src/js/modules/table.js
@@ -1,10 +1,10 @@
-//import {gridColumns,tableDataOne} from './data/data.js';
+import {gridColumns,tableDataOne} from './data/data.js';
 
 export function table (idTable, srcTable, gridTable) {
     return {
         view:"datatable",
-        id:idTable, 
-        data:srcTable,
-        columns: gridTable
+        id:"tableOne", 
+        data:tableDataOne,
+        columns: gridColumns
     };
 }
